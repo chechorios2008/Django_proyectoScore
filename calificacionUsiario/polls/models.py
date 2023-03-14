eafb43a17c3ca3d611f97fdb39ed663cccb4db43
checho rios <chechorios2008@gmail.com>
2023-03-14 00:27:40 -0500

    Creación de datos desde la consola interactiva de Django

--- calificacionUsiario/polls/models.py
+++ calificacionUsiario/polls/models.py
@@ -1,10 +1,23 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateField("Date published")
 
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+#TimeDelta => Define la diferencia de tiempo.    
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choise_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
