5833e0fc5d37f43b55b86d1e1bb2f7c1d1c2dc75
checho rios <chechorios2008@gmail.com>
2023-03-15 00:25:26 -0500

    Creación de las primeras vistas

--- calificacionUsiario/polls/views.py
+++ calificacionUsiario/polls/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . models import Question
+
 def index(request):
-    return HttpResponse("Hello World")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list":latest_question_list
+    })
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta número {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votanto a la pregunta número {question_id}")
